eabd0d602c9abe533ae8592a5fd0d3d0c417d16b
FabianRaja <fabiraja21052002@gmail.com>
2024-01-16 13:53:56 +0530

    fixed

--- src/Helper/api.jsx
+++ src/Helper/api.jsx
@@ -1,2 +1,2 @@
-// export const API="https://urlshortener-yiq2.onrender.com";
-export const API="http://localhost:9000";
+export const API="https://urlshortener-yiq2.onrender.com";
+// export const API="http://localhost:9000";
